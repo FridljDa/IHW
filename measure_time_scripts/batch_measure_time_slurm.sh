af8853b9d3ede48b72ed5ee1a110bd637e9a035e
Daniel Fridljand <daniel.fridljand@gmail.com>
2023-06-28 16:04:13 +0200

    trying to submit a batch job

---
+++ measure_time_scripts/batch_measure_time_slurm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+m=1000
+r=2
+ntrees=2
+
+
+# Run R script
+sbatch measure_time_scripts/job_measure_time_slurm.sh $m $r $ntrees
---
+++ measure_time_scripts/job_measure_time_slurm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Example of running R script with a job array
+
+#SBATCH -A huber                # group to which you belong
+#SBATCH -N 1                        # number of nodes
+#SBATCH -n 3                        # number of cores
+#SBATCH --mem 5                    # memory pool for all cores
+#SBATCH -t 0-1:00                   # runtime limit (D-HH:MM:SS)
+#SBATCH -o measure_time_scripts/out_measure_time/measure_time_out-%j.out
+#SBATCH -e measure_time_scripts/err_measure_time/measure_time_er-%j.err          # STDERR
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT_50,TIME_LIMIT_80,TIME_LIMIT_90        # notifications for job done & fail
+#SBATCH --mail-user=daniel.fridljand@embl.de # send-to address
+# Load software
+
+m=$1
+r=$2
+ntrees=$3
+
+module load R
+# Run R script
+Rscript --vanilla measure_time_scripts/measure_time.R $m $r $ntrees
